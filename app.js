a65cd827b8993d9b2525f6f2f618607beedf5a00
aromalkallingal <aromalkallingal5@gmail.com>
2022-09-15 23:40:07 +0530

    rendered a string on dom

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 let gameBoardModule = (function() {
-    let gameBoard = [];
-    return{};
-})
+    let gameBoard = ["X"];
+    return {gameBoard};
+})();
+
 
 let displayControllerModule = (function() {
     let testF = () => {console.log("testing private function call inside of a module object.....")};
-
+    return {testF};
 })();
 
 let createPlayer = (playerName, playerNumber, assignedXO) => {
-    let playerName = () => {playerName;
+    let getPlayerName = () => {playerName;
     console.log("this is the name of the player" + playerNumber + "....." + playerName);}
     return {getPlayerName, playerName, playerNumber, assignedXO};
 };
 
+let renderArrayToScreenModule = (function() {
+    let gridBoxes = document.querySelectorAll(".grid-box");
+    gridBoxes[0].textContent = gameBoardModule.gameBoard;
+    console.log("show me the array", gameBoardModule.gameBoard);
+    console.log("show me node list", gridBoxes);
+    return{};
+})();
+
 let Justin = createPlayer("Justin", 1, "X");
 let James = createPlayer("James", 2, "O");
+renderArrayToScreenModule;
+
