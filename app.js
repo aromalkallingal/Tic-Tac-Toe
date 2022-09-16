9222dc9207fda340fc0b6bb386c0bb42ca5325f0
aromalkallingal <aromalkallingal5@gmail.com>
2022-09-17 00:21:22 +0530

    version 2 hard coded

--- app.js
+++ app.js
@@ -5,25 +5,42 @@ let gameBoardModule = (function() {
 
 
 let displayControllerModule = (function() {
-    let testF = () => {console.log("testing private function call inside of a module object.....")};
+    const makeMove = document.querySelectorAll(".game-board-button");
+
+    let index = 0;
+    makeMove.forEach(makeMoves => {
+        makeMoves.dataset.linkedButton = index;
+        makeMoves.addEventListener("click", renderArrayToScreen);
+
+        function renderArrayToScreen() {
+            const gridBoxes = document.querySelectorAll(".grid-box");
+
+            let index = 0;
+            gridBoxes.forEach(gridBox => {
+                gridBox.dataset.linkedButton = index;
+
+                if (gridBox.getAttribute("data-linked-button") ==makeMoves.getAttribute("data-linked-button")) {
+                    gridBox.textContent = gameBoardModule.gameBoard[gameBoardModule.gameBoard.length - 1];
+                    console.log(makeMoves.dataset.linkedButton);
+                    console.log(gridBox.dataset.linkedButton);
+                }
+            index++;
+            })
+        }
+    index++;
+    })
+
+    let testF = () => {console.log("testing")};
     return {testF};
+
 })();
+
 
 let createPlayer = (playerName, playerNumber, assignedXO) => {
-    let getPlayerName = () => {playerName;
-    console.log("this is the name of the player" + playerNumber + "....." + playerName);}
-    return {getPlayerName, playerName, playerNumber, assignedXO};
+    let getPlayerName = () => { playerName;
+        console.log(playerName + "" + playerName); }
+        return {getPlayerName, playerName, playerNumber, assignedXO};
 };
-
-let renderArrayToScreenModule = (function() {
-    let gridBoxes = document.querySelectorAll(".grid-box");
-    gridBoxes[0].textContent = gameBoardModule.gameBoard;
-    console.log("show me the array", gameBoardModule.gameBoard);
-    console.log("show me node list", gridBoxes);
-    return{};
-})();
 
 let Justin = createPlayer("Justin", 1, "X");
 let James = createPlayer("James", 2, "O");
-renderArrayToScreenModule;
-
